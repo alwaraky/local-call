7c36b0b42fed2fa050e1c0113210dcb810c2edf7
alwaraky <worldvacuum2019@gmail.com>
2024-06-26 21:58:39 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+﻿const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    // إرسال الرسالة إلى جميع العملاء المتصلين
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('WebSocket server is running on ws://192.168.1.5:8080');
